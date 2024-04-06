f4c5ca68d5bf3accc30abde6833eb679dfaee318
Gabe Piano <43084571+Poije@users.noreply.github.com>
2024-04-06 23:56:59 +0800

    update main.js

--- ThreeJs/main.js
+++ ThreeJs/main.js
@@ -3,6 +3,10 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const loader = new GLTFLoader();
+/*const textureLoader = new THREE.TextureLoader();
+textureLoader.load('Autobots.png', function(texture){
+    scene.background = texture;
+}); */
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -10,23 +14,34 @@ renderer.setClearColor(0xeeeeee);
 document.body.appendChild( renderer.domElement );
 
 let object;
-let camera;
+let camera
+let mixer;
 
-loader.load( 'scene.gltf', function ( gltf ) {
+
+loader.load( 'scene_with_anims.gltf', function ( gltf ) {
     object = gltf.scene;
     scene.add( object );
     camera = gltf.cameras[0];
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
+   mixer = new THREE.AnimationMixer(object);
+    gltf.animations.forEach((clip) => {
+        mixer.clipAction(clip).play();
+    });
+
 },
+
 function(xhr){
     console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 }, function ( error ) {
     console.error( error );
 } );
 
-
+const clock = new THREE.Clock();
 function animate() {
+    if (mixer){
+        mixer.update(clock.getDelta());
+    }
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 }
